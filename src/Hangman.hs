74fc4f617953a7eda5544d39e2a8e51d3e422e1d
Frantisek Kocun <frantisek.kocun@merchenta.com>
2016-01-26 00:03:48 -0500

    working game + get random words from url

--- src/Hangman.hs
+++ src/Hangman.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
 module Hangman where
 
 import Control.Monad
 import Data.List (find)
 import Data.Char (toLower)
 import Data.Maybe (fromJust)
--- import Debug.Trace
+import Streaming
+import qualified Streaming.Prelude as S
+--import Control.Monad.State
+--import Control.Monad.Identity
+import Network.HTTP
 
 data GameState = GameState {
   secretWord :: String,
@@ -19,8 +23,8 @@ newGameState word = GameState (map toLower word) 5 []
 showWord state = map (\c -> if elem c (guesses state) then c else '_') (secretWord state)
 showState s = "You have " ++ (show $ lives s) ++ " lifes. The word is \"" ++ (showWord s) ++ "\""
 
-updateState :: GameState -> Char -> GameState
-updateState s c = if elem c (secretWord s) 
+updateGameState :: GameState -> Char -> GameState
+updateGameState s c = if elem c (secretWord s) 
   then s {guesses = c : (guesses s)}
   else s {lives = (lives s) - 1}
 
@@ -40,8 +44,8 @@ gameInProgress = not . gameEnded
 
 ---------- Version of game to test game logic. Pass [Chars] as user input ----------
 -- runGameTesting (newGameState "secret") "secrfta"
-runGameTesting :: GameState -> [Char] -> [GameState]
-runGameTesting gs stream = takeWhile gameInProgress $ scanl updateState gs stream
+runGameTesting :: GameState -> [Char] -> GameState
+runGameTesting gs stream = head $ dropWhile gameInProgress $ scanl updateGameState gs stream
 
 
 -- gets a letter from terminal
@@ -60,39 +64,63 @@ runGameRecursively gs =
   then putStrLn $ showState gs
   else do
     l <- getALetter gs
-    runGameRecursively $ updateState gs l
+    runGameRecursively $ updateGameState gs l
 
 ---------- Infitine list version of game ----------
 -- runGameIntinite (newGameState "secret")
-updateGameState :: GameState -> IO GameState
-updateGameState gs = do
+updateGameStateIO :: GameState -> IO GameState
+updateGameStateIO gs = do
    l <- getALetter gs
-   return $ updateState gs l
+   return $ updateGameState gs l
 
-ff :: (a -> Bool) -> [IO a] -> IO a
-ff f (i:is) = do
-  res <- i
-  if f res then return res else ff f is
+--ioStream :: Stream (S.Of GameState) IO ()
+--ioStream = S.iterateM updateGameState (return gs)
 
-fff :: (a -> Bool) -> [IO a] -> IO a
-fff f (i:is) = do
-  res <- i
-  if f res then return res else ff f is
-
-iii !x = x >>= updateGameState
-
-runGameInfinite :: GameState -> IO ()
-runGameInfinite gs =
-  -- infinite lazy game loop
-  let repl = tail $ iterate (\x -> x >>= updateGameState) (return gs) :: [IO GameState]
-  in do
-    endState <- fff gameEnded repl
---     endState <- last $ take 3 repl
---     endState <- fmap fromJust $ liftM (find gameEnded) (sequence repl)
-    putStrLn $ showState endState
+runGameInfiniteStream ::  Monad m => (GameState -> m GameState) -> GameState -> Stream (S.Of GameState) m ()
+runGameInfiniteStream updateGameStateFunction gs = S.take 1 $ S.dropWhile gameInProgress steps
+   where
+   --steps :: Monad m => GameState -> (GameState -> m GameState) -> Stream (S.Of GameState) m ()
+   steps = S.iterateM updateGameStateFunction (return gs)
 
 ---------- Main ----------
 main :: IO ()
-main = runGameInfinite (newGameState "car")
+-- main = putStrLn ""
+main = do
+  word <- randomWord
+  S.print $ runGameInfiniteStream updateGameStateIO (newGameState word)
 
+--s :: [Char] -> GameState -> State [Char] GameState
+--s = undefined
+
+----m = runGameInfiniteStream (updateGameStateTest "asdafsdsa") (newGameState "car")
+
+--updateGameStateTest :: MonadState [Char] m => [Char] -> GameState -> m GameState
+--updateGameStateTest = undefined
+--updateGameStateTest gs = 
+--  (do (l:ls) <- get
+--      put ls
+--      return (updateGameState gs l)
+--  )
+
+--updateGameStateTest2 :: [Char] -> GameState -> Identity GameState
+--updateGameStateTest2 (x:xs) gs = return $ updateGameState gs x
+
+--mainn = S.print $ runGameInfiniteStream (updateGameStateTest "") (newGameState "car")
+
+get :: String -> IO String
+get url = simpleHTTP (getRequest url) >>= getResponseBody
+
+randomWord = get "http://randomword.setgetgo.com/get.php"
+
+type Stack = [Int]  
+  
+pop :: Stack -> (Int,Stack)  
+pop (x:xs) = (x,xs)  
+  
+push :: Int -> Stack -> ((),Stack)  
+push a xs = ((),a:xs)  
+stackManip stack = let  
+    ((),newStack1) = push 3 stack  
+    (a ,newStack2) = pop newStack1  
+    in pop newStack2  
 
--- src/Main.hs
+++ src/Main.hs
@@ -3,12 +3,3 @@ module Main where
 import qualified Hangman (main)
 
 main = Hangman.main
-
-
-
--- -- let l = [1, 2, 3, 4, 1, 1, 1, 1]
--- -- Prelude> scanl (+) 0 l
--- -- [0,1,3,6,10,11,12,13,14]
--- -- Prelude> takeWhile (< 12) $ scanl (+) 0 l
--- -- [0,1,3,6,10,11]
-
